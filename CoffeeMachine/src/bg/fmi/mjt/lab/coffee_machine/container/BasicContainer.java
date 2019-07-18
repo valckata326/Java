bbeba09c9506bd92d67bf4a2b5559109e7b99738
valckata326 <valentin.ilievp@gmail.com>
2019-07-18 11:44:52 +0300

    reorder

---
+++ CoffeeMachine/src/bg/fmi/mjt/lab/coffee_machine/container/BasicContainer.java
@@ -0,0 +1,75 @@
+package bg.fmi.mjt.lab.coffee_machine.container;
+
+import bg.fmi.mjt.lab.coffee_machine.container.Container;
+import bg.fmi.mjt.lab.coffee_machine.supplies.Beverage;
+
+public class BasicContainer extends Container {
+
+
+    private  final double max_coffee = 600;
+    private  final double max_water = 600;
+
+    private double currentCoffee;
+    private double currentWater;
+
+    public BasicContainer()
+    {
+        this.setCurrentCoffee(getMax_coffee());
+        this.setCurrentWater(getMax_water());
+    }
+
+    public void refill()
+    {
+        this.setCurrentWater(this.getMax_water());
+        this.setCurrentCoffee(this.getMax_coffee());
+    }
+
+    @Override
+    public double getCurrentCacao() {
+        return 0;
+    }
+
+    @Override
+    public double getCurrentMilk() {
+        return 0;
+    }
+
+    @Override
+    public double getCurrentCoffee() {
+        return currentCoffee;
+    }
+
+    @Override
+    public double getCurrentWater() {
+        return currentWater;
+    }
+
+    public void updateSupplies(Beverage beverage)
+    {
+        setCurrentCoffee(currentCoffee-beverage.getCoffee());
+        setCurrentWater(currentWater-beverage.getWater());
+    }
+
+    private void setCurrentCoffee(double currentCoffee) {
+        if(currentCoffee >= 0 && currentCoffee <= getMax_coffee())
+        {
+            this.currentCoffee = currentCoffee;
+        }
+
+    }
+
+    private void setCurrentWater(double currentWater) {
+        if(currentWater >= 0 && currentWater <= getMax_water())
+        {
+            this.currentWater = currentWater;
+        }
+    }
+
+    public double getMax_coffee() {
+        return max_coffee;
+    }
+
+    public double getMax_water() {
+        return max_water;
+    }
+}
---
+++ CoffeeMachine/src/bg/fmi/mjt/lab/coffee_machine/container/Container.java
@@ -0,0 +1,10 @@
+package bg.fmi.mjt.lab.coffee_machine.container;
+
+public abstract class Container {
+
+    public abstract double getCurrentWater();
+    public abstract double getCurrentCoffee();
+    public abstract double getCurrentMilk();
+    public abstract double getCurrentCacao();
+}
+
