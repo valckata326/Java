bd09b2a0529388e5be23c6d08efede10e04e50b7
valckata326 <valentin.ilievp@gmail.com>
2019-07-18 11:47:49 +0300

    remove src

--- src/bg/fmi/mjt/lab/coffee_machine/supplies/Beverage.java
+++
@@ -1,9 +0,0 @@
-package bg.fmi.mjt.lab.coffee_machine.supplies;
-
-public interface Beverage {
-    public String getName();
-    public double getCoffee();
-    public double getWater();
-    public double getMilk();
-    public double getCacao();
-}
