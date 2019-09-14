57051babe1f9e6460218ef98f40e6c9cc9b53beb
Nimor111 <georgi.bojinov@hotmail.com>
2019-09-14 14:32:04 +0300

    Leave only Christmas task

--- StyleChecker/src/bg/sofia/uni/fmi/mjt/stylechecker/Main.java
+++
@@ -1,8 +0,0 @@
-package bg.sofia.uni.fmi.mjt.stylechecker;
-
-public class Main {
-    public static void main(String[] args)
-    {
-        //StyleChecker style = new StyleChecker(null);
-    }
-}
