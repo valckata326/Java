1a0daf2cf50de08b13e55180c5efff9117e02f83
Nimor111 <georgi.bojinov@hotmail.com>
2019-09-14 14:34:21 +0300

    Leave only Movies task

--- Cache/src/bg/sofia/uni/fmi/mjt/cache/CapacityExceededException.java
+++
@@ -1,7 +0,0 @@
-package bg.sofia.uni.fmi.mjt.cache;
-
-public class CapacityExceededException extends Exception {
-    public CapacityExceededException(String string) {
-        super(string);
-    }
-}
